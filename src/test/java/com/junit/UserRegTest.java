c38c079f5cae8fa155317606f6e1d6eb45899710
Rachappa-Minajagi <m.rachoti142@gmail.com>
2022-03-13 11:40:04 +0530

    Added:Minmum 8 char password

--- src/test/java/com/junit/UserRegTest.java
+++ src/test/java/com/junit/UserRegTest.java
@@ -5,12 +5,9 @@ import org.junit.Test;
 import org.junit.Assert;
 import org.junit.Test;
 
-/*
- * 
-/**
- * UC3- As a User need to enter a valid email 
- * -E.g. abc.xyz@bl.co.in - Email has 3 mandatory parts (abc, bl& co)
- *
+/*UC5_Password
+ * As a User need to follow pre-defined Password 
+ * rules.Rule1– minimum 8 Characters - NOTE – All rules must be passed
  */
 public class UserRegTest {
 
@@ -69,5 +66,47 @@ public class UserRegTest {
 
 	public boolean checkEmail(String emailID) {
 		return (emailID.matches("^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*[@][0-9A-Za-z]+([.][a-zA-Z]{2,4})*$"));
+	}
+
+	public void testPhoneNumber_MustReturnTrue() {
+		UserRegistration userRegistration = new UserRegistration();
+		/**
+		 * calling checkPhoneNum method to see phoneNumber is valid
+		 */
+		boolean isPhoneNumberValid = userRegistration.checkPhoneNum("91 9686127142");
+		Assert.assertTrue(isPhoneNumberValid);
+	}
+
+	@Test
+	/**
+	 * created testPhoneNumber_MustReturnFalse() for False condition
+	 */
+	public void testPhoneNumber_MustReturnFalse() {
+		UserRegistration userRegistration = new UserRegistration();
+		boolean isPhoneNumberInValid = userRegistration.checkPhoneNum("9686127142");
+		Assert.assertFalse(isPhoneNumberInValid);
+	}
+
+	@Test
+	/*
+	 * created testPassword_MustReturnTrue() for True condition
+	 */
+	public void testPassword_MustReturnTrue() {
+		UserRegistration userRegistration = new UserRegistration();
+		/**
+		 * calling checkPassword method to see Password is valid
+		 */
+		boolean isPasswordValid = userRegistration.checkPassword("abcdefghi");
+		Assert.assertTrue(isPasswordValid);
+	}
+
+	@Test
+	/*
+	 * created testPassword_MustReturnFalse() for False condition
+	 */
+	public void testPassword_MustReturnFalse() {
+		UserRegistration userRegistration = new UserRegistration();
+		boolean isPasswordInValid = userRegistration.checkPassword("abcd");
+		Assert.assertFalse(isPasswordInValid);
 	}
 }
