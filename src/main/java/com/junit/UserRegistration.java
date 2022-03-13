5ff773ad13518fa39b64adb517734456d7656d96
Rachappa-Minajagi <m.rachoti142@gmail.com>
2022-03-13 10:56:45 +0530

    Added: laststName

--- src/main/java/com/junit/UserRegistration.java
+++ src/main/java/com/junit/UserRegistration.java
@@ -1,12 +1,19 @@
 package com.junit;
-
 /*
  * As a User need to enter a valid First Name
  * First name starts with Cap and has minimum 3 characters
- */
+ */        
 public class UserRegistration {
 	public boolean checkFName(String fName) {
 		return (fName.matches("[A-Z][a-z]{3,}"));
 
+	}
+	
+	/*
+	 *   As a User need to enter a valid lirstName
+	 *    firstName starts with Cap and has minimum 3 characters
+	 */
+	public boolean Lname(String Lname) {
+		return (Lname.matches("[A-Z][a-z]{3,}"));
 	}
 }
